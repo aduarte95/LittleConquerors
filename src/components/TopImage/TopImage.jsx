0eb064f8e29d9888e059ea408b1d94e0668afc35
Alexa Duarte <17975101+aduarte95@users.noreply.github.com>
2020-02-11 00:12:32 -0600

    Revert "Development"

--- src/components/TopImage/TopImage.jsx
+++ src/components/TopImage/TopImage.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import './TopImage.scss';
 
-
-
-function TopImage({member, cont, totalFacts, setCont}) {
-
-  function showFact(totalFacts, cont, setCont) {
-    if(cont < totalFacts) {
-        setCont(cont + 1);
-    }  
-  }
-
+function TopImage({member}) {
+      
   return (   
     <div className="row justify-content-center">
-      <div className="top-img-container__wrapper">
-        <img onClick={() => showFact(totalFacts, cont, setCont) } className="top-img-container__img" src={member.imgUrl} alt={member.name}/>
-        {cont === 0 && <p className="top-img-container__click-me">Click me!</p>}    
-      </div>
+        <img className="top-img-container__img" src={member.imgUrl} alt={member.name}/>
     </div>
   );
 }
